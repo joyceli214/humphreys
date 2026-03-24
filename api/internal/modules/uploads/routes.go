a61d2e08484f7e1b6eb77149b3c30ae527fda213
joyceli214 <joyceli214@yahoo.com.hk>
2026-03-23 23:35:53 -0400

    update permission

--- api/internal/modules/uploads/routes.go
+++ api/internal/modules/uploads/routes.go
@@ -1,14 +1,10 @@
 package uploads
 
 import (
-	"humphreys/api/internal/middleware"
-
 	"github.com/gin-gonic/gin"
 )
 
-const permUploadsCreate = "uploads:create"
-
 func RegisterRoutes(authed *gin.RouterGroup, h *Handler) {
-	authed.POST("/uploads/markdown-image", middleware.RequirePermission(permUploadsCreate), h.UploadMarkdownImage)
-	authed.DELETE("/uploads/markdown-image", middleware.RequirePermission(permUploadsCreate), h.DeleteMarkdownImage)
+	authed.POST("/uploads/markdown-image", h.UploadMarkdownImage)
+	authed.DELETE("/uploads/markdown-image", h.DeleteMarkdownImage)
 }
